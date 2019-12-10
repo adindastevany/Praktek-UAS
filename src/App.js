8fa43d28d677d2da15f12179a58a71d71f4216c8
Adinda Putri Stevany 18 <adindaputristevani@gmail.com>
2019-12-10 14:19:50 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "./App.css";
+
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className=" wrapper with-image " >
+                <div className=" Component1">
+                    <div className="teks1"><h4>GO GREEN</h4></div>
+                    <div className="teks2"><h1><b>Make it green</b> </h1></div>
+                </div>
+
+                <div className="teks3">
+                    <p>
+                        Now more than ever. Our planet is need our
+                    </p>
+                </div>
+
+                <div className="Component2">
+                    <div className="teks-itu">Do it Now</div>
+                </div>
+
+
+                <div className="right">
+                    <i class="path"></i>
+                </div>
+
+            </div>
+        )
+    }
+}
+//function App(){
+//return(
+
+//):
+//}
+export default App;
